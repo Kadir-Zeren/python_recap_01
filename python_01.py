d39847d83eacb9271e4d87c6e75e7be6963f9ac1
kadir-zeren <k4dzeren@gmail.com>
2024-10-28 05:00:53 +0300

    practice

--- python_01.py
+++ python_01.py
@@ -1,2 +1,150 @@
 print("Hello World !")
 print( "hello world")
+
+print ("hello")
+
+print("Hello world")
+print('Benim yazdigim ilk kod')
+print('Hello World!')
+
+print()
+print('ben bunu yazmak istiyorum')
+
+print("Hello World")
+print("Selam")
+
+print('Hello World! ')
+
+print('Hello World!')
+print("Hello World!")
+
+print('Hello World! ')
+print("Clarusway School!")
+print("I'm happy to learn!")
+
+print("Cift tirnak yazdim")
+print('''uc tirnak ile yazdim''')
+
+print("I'm Great")
+print("bir cumle")
+print("I'm great")
+
+print('"hi" he said')
+print('''"hi" he said''')
+
+print('"hi" he said')
+
+print('''"Don't do it" said the manager''')
+print('''Don't do it" said the manager''')
+
+print('''Lorem ipsum dolor sit amet,
+fusce ut quisque neque donec,
+massa metus amet, luctus inceptos.''')
+
+print("""Praesent a, mi mattis velit metus,
+accumsan adipiscing ipsum sit justo
+penatibus, amet mauris non tempus justo.""")
+
+print("ben bunu yazmak istiyorum")
+
+print()
+
+print('''bu birinci satir
+bu ikinci satir
+bu uc
+bu dort''')
+
+print('''bu birinci 'satir
+bu ikinci satir"
+bu uc
+bu dort''')
+
+print('3.14')
+print(3.14)
+
+print('3.14')
+print(3.14)
+
+print('3.14'*3)
+print(3.14*3)
+
+'abcd' * 2
+
+5 * 2
+
+print(567)
+
+print(''''We should have enough time for our family"''')
+
+print('first line')
+print()
+print('''third line''')
+
+print('bir')
+print()
+print('uc')
+
+print()
+print('')
+
+print()
+print("")
+
+print('I\'m great')
+
+print("\"don't do it\" said the manager")
+
+print("bu benim cumlem \nbu ikinci satir")
+
+print("bu benim cumlem \tbu ikinci satir")
+
+A = 3
+print ( 'A =', A)
+print ( 'A = %d' %A)#C style
+print ( "A = {0} ". format (A) )
+print (f"A = {A} ")
+print ( 'A = ' + str ( A ) )#Java style
+
+print('being a good person')
+
+print("ben kodlarkjlj vl k k k kk ll 1kjlkjlkjljlk \
+kdkdkdkdkdkd\
+kdkdkdkdkd")
+
+x = 3
+y = 4
+long_vars = 5
+
+x =3
+y =4
+long_vars = 5
+
+# buradaki fonksiyon kare alma fonksiyonu
+
+print("kullanici ismi") # Burada kullanici ismi yazddiriliyor.
+
+x = 5 # burada x degeri atandi
+
+x = 10
+y = 5
+a = (x / y) * 25
+print(a ** 2)
+
+X = 10
+y = 5
+a = (x / y) * 100
+print(a)
+
+# This is a single line comment
+
+print('hello') # This is an inline comment
+
+print('hello')
+# First multi-line comment
+# Second multi-line comment
+# Third multi-line comment
+
+print('Hello', "world")
+
+print('Hello, world')
+print("Hello, world!")
